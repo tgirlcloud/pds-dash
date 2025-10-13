4871dbfa2ddb42ef82fee3bfff2f59e7ccd268f6
isabel <isabel@isabelroses.com>
2025-10-13 16:43:10 +0100

    fix: small fixes here and there

--- shell.nix
+++ shell.nix
@@ -1,6 +1,9 @@
-# girl who's shell isn't reproducible
 {
-  pkgs ? import <nixpkgs> { },
+  nixpkgs ? builtins.fetchTarball {
+    url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
+    sha256 = "sha256-tmsZd73cUKgmJROwHYxIMxjOoQ34qmJWQ6X6K+F7V14=";
+  },
+  pkgs ? import nixpkgs { },
 }:
 pkgs.mkShell {
   packages = with pkgs; [
