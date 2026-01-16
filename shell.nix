b756931d6c25625031264e3b78fe4d560a6cb19e
isabel <isabel@isabelroses.com>
2026-01-16 14:34:41 +0000

    chore: update nix shell

--- shell.nix
+++ shell.nix
@@ -1,14 +1,14 @@
 {
   nixpkgs ? builtins.fetchTarball {
     url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
-    sha256 = "sha256-mU85VDFRIgKGq1EhT71bLjhvjJ5yuMEe0Ip1kwCbR80=";
+    sha256 = "sha256-wufp5c0nWh/87f9eK7xy1eZXms5zd4yl6S4SR+LfA08=";
   },
   pkgs ? import nixpkgs { },
 }:
 pkgs.mkShell {
   packages = with pkgs; [
     pnpm
-    nodejs-slim_24
+    nodejs-slim
     typescript-language-server
   ];
 }
