0c6bda4bd825971d55c5f3edb72512fac40c1a6e
isabel <isabel@isabelroses.com>
2026-02-26 15:25:20 +0000

    chore: bump deps

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 {
   nixpkgs ? builtins.fetchTarball {
     url = "https://channels.nixos.org/nixpkgs-unstable/nixexprs.tar.xz";
-    sha256 = "sha256-wufp5c0nWh/87f9eK7xy1eZXms5zd4yl6S4SR+LfA08=";
+    sha256 = "sha256-lJiQtL47xYV37Vod9KgPS7dmi1x51A5+chJE48qKimw=";
   },
   pkgs ? import nixpkgs { },
 }:
